0b17c7e13e0438ac02f0c7da34ee47157426af1e
Robin.D <chilimannen93@yahoo.com>
2014-12-08 22:08:25 +0100

    User Class Removed

--- src/model/Review.java
+++ src/model/Review.java
@@ -2,22 +2,22 @@ package model;
 
 public class Review {
  private String text;
- private User user; //? or text.
+ private String user; //? or text.
 public String getText() {
 	return text;
 }
 public void setText(String text) {
 	this.text = text;
 }
-public User getUser() {
+public String getUser() {
 	return user;
 }
-public Review(String text, User user) {
+public Review(String text, String user) {
 	super();
 	this.text = text;
 	this.user = user;
 }
-public void setUser(User user) {
+public void setUser(String user) {
 	this.user = user;
 }
  
