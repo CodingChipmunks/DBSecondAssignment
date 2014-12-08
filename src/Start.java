87118a2ffb4542d6348e7d2608468a0489e72393
Robin.D <chilimannen93@yahoo.com>
2014-12-08 18:18:01 +0100

    Push Test
    test test ??

--- src/Start.java
+++ src/Start.java
@@ -7,6 +7,7 @@ public class Start {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// deez pulls
 		// instantiate model
 		// feed model to view when instantiating it
 		Model m = new Model();
