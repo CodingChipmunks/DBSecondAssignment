e5296b1af2c065c765cf8a3e07aff3acb08c443c
softish <lazar.h.klincov@live.se>
2014-12-09 21:25:54 +0100

    basix of actionHandling implemented

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,6 +3,7 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
 import javax.swing.event.ChangeListener;
 
 import model.*;
@@ -12,7 +13,7 @@ import view.*;
  * Handles listeners
  * 
  * @author softish
- *
+ * 
  */
 public class Controller implements ActionListener {
 
@@ -20,28 +21,58 @@ public class Controller implements ActionListener {
 	private View view;
 	private Model model;
 	private QueryInterpreter dbInterpreter;
-	
+	private int selectedItemInCombobox;
+
 	public Controller(Model m, View v) {
 		this.model = m;
 		this.view = v;
 	}
-	
+
 	public Controller(Model m, WBView wbv) {
 		this.model = m;
 		this.wbview = wbv;
 	}
 
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 
+		// Works great, but unusable
+		if (e.getActionCommand() == "comboBox") {
+			System.out.println("Checking mediaType...");
+
+			JComboBox cb = (JComboBox) e.getSource();
+			String item = (String) cb.getSelectedItem();
+			System.out.println("Item " + item);
+
+		}
 		
+		if (e.getActionCommand().equals("searchBtn")) {
+			System.out.println("Searching...");
+
+			// Dangerous solution but works!
+			JComboBox cb = wbview.getSearchPanelComponent();
+			String item = (String) cb.getSelectedItem();
+			System.out.println("Item " + item);
+
+			// selectedItemInCombobox = wbview.getSelectedItem();
+			// System.out.println("Selected mediaType..." +
+			// selectedItemInCombobox);
+		}
 		
+		if (e.getActionCommand().equals("rateBtn")) {
+			System.out.println("Rating dialog...");
+		}
 		
-		//end with //view.updateView();
+		if (e.getActionCommand().equals("reviewBtn")) {
+			System.out.println("Review dialog...");
+		}
+		
+		if (e.getActionCommand().equals("addBtn")) {
+			System.out.println("Adding dialog...");
+		}
+
+		// end with //view.updateView();
 	}
 
-
-	
 }
--- src/view/WBView.java
+++ src/view/WBView.java
@@ -13,15 +13,17 @@ import model.QueryInterpreter;
 
 /**
  * WindowBuilder class, compatible with git? Better than Start?
- *
+ * 
  */
 public class WBView extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
 	private JTextField textField;
-	private String[] searchOptions = {"Album", "Movie", "E-Book"};
-	
+	private JComboBox comboBox;
+	private JPanel searchPanel;
+	private String[] searchOptions = { "Album", "Movie", "E-Book" };
+
 	// declare all GUI components here
 
 	/**
@@ -29,52 +31,94 @@ public class WBView extends JFrame {
 	 */
 	public WBView(Model m) {
 		Controller controller = new Controller(m, this);
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		table = new JTable();
 		contentPane.add(table, BorderLayout.CENTER);
-		
+
 		JPanel btnPanel = new JPanel();
 		contentPane.add(btnPanel, BorderLayout.SOUTH);
-		
+
 		JButton rateBtn = new JButton("Rate Selected");
+		rateBtn.setActionCommand("rateBtn");
+		rateBtn.addActionListener(controller);
 		btnPanel.add(rateBtn);
-		
+
 		JButton reviewBtn = new JButton("Review Selected");
+		reviewBtn.setActionCommand("reviewBtn");
+		reviewBtn.addActionListener(controller);
 		btnPanel.add(reviewBtn);
-		
+
 		JButton addBtn = new JButton("Add Media");
+		addBtn.setActionCommand("addBtn");
+		addBtn.addActionListener(controller);
 		btnPanel.add(addBtn);
-		
-		JPanel searchPanel = new JPanel();
+
+		searchPanel = new JPanel();
 		contentPane.add(searchPanel, BorderLayout.NORTH);
 		searchPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		textField = new JTextField();
 		searchPanel.add(textField, BorderLayout.NORTH);
 		textField.setColumns(10);
-		
+
 		JButton searchBtn = new JButton("Search");
+		searchBtn.setActionCommand("searchBtn");
+		searchBtn.addActionListener(controller);
 		searchPanel.add(searchBtn, BorderLayout.EAST);
-		
+
 		JComboBox comboBox = new JComboBox(searchOptions);
 		comboBox.setActionCommand("comboBox");
 		comboBox.setPrototypeDisplayValue("as long as this");
 		comboBox.setSelectedIndex(0);
 		comboBox.addActionListener(controller);
 		searchPanel.add(comboBox, BorderLayout.WEST);
-		
 
 		// create components in methods
+
+	}
+
+	// PArt of the dangerous solution
+	public JComboBox getSearchPanelComponent() {
+		return (JComboBox) searchPanel.getComponent(2);
+	}
+
+	// unusable ref.
+	// public JComboBox getComboBox() {
+	// return comboBox;
+	// }
+
+	// unusable ref.
+	// public int getSelectedItem() {
+	// return comboBox.getSelectedIndex();
+	// }
+
+	public void feedTable() {
 		
 	}
 	
+	public void invokeReviewMediaDialog() {
+
+	}
+
+	public void invokeRateMediaDialog() {
+
+	}
+
+	public void invokeAddMediaDialog() {
+
+	}
+
+	public String getSearchFieldText() {
+		return textField.getText();
+	}
+
 	public void updateView() {
 		contentPane.updateUI();
 	}
