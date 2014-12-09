e1b196cb8c7c4d2a653788f6f16a4797747c4951
Robin.D <chilimannen93@yahoo.com>
2014-12-09 20:59:41 +0100

    Updated SQL Scripts

--- Build-Database.sql
+++ Build-Database.sql
@@ -10,6 +10,19 @@ USE MediaCollection;
 
 -- Generate tables
 
+-- Create User, Contains password and username for use in the post-review/post-rating procedures.
+CREATE TABLE IF NOT EXISTS Account(
+		Name VARCHAR(24) NOT NULL, 
+        Pass VARCHAR(24), 
+	PRIMARY KEY (Name)
+);
+
+CREATE TABLE IF NOT EXISTS User(
+		Id INTEGER AUTO_INCREMENT PRIMARY KEY,
+		Account_Name VARCHAR(24),
+	FOREIGN KEY(Account_Name) REFERENCES Account(Name) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- Create Director/Artist
 CREATE TABLE IF NOT EXISTS Creator(
 		Id INTEGER AUTO_INCREMENT, 
@@ -43,13 +56,15 @@ INSERT INTO Genre (Name) VALUES ("Undefined"); -- Genre defaults to undefined.
 CREATE TABLE IF NOT EXISTS Media(
 		Id INTEGER AUTO_INCREMENT, 
         Mediatype_Id INTEGER, 
-        Genre INTEGER DEFAULT 1, 
+        Genre_Id INTEGER DEFAULT 1, 
         Title VARCHAR(24) NOT NULL, 
         Year INTEGER, 
         Duration INTEGER, 
+        User_Id INTEGER,
 	PRIMARY KEY (Id), 
 	FOREIGN KEY (Mediatype_Id) REFERENCES Mediatype(Id),
-    FOREIGN KEY (Genre) REFERENCES Genre(Id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (User_Id) REFERENCES User(Id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (Genre_Id) REFERENCES Genre(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
     
 -- Add Indexing Media on Media Type and Title
@@ -64,27 +79,22 @@ CREATE TABLE IF NOT EXISTS Contributor(
 	FOREIGN KEY (Media_Id) REFERENCES Media(Id) ON DELETE CASCADE ON UPDATE CASCADE 
 );
 
--- Create User, Contains password and username for use in the post-review/post-rating procedures.
-CREATE TABLE IF NOT EXISTS User(
-		Name VARCHAR(24) NOT NULL, 
-        Pass VARCHAR(24), 
-	PRIMARY KEY (Name)
-);
-
 -- Create Rating, Username is unique so that every user may only create one rating.  
 CREATE TABLE IF NOT EXISTS Rating(
 		Media_Id INTEGER, 
-		Username VARCHAR(24) UNIQUE, 
+		User_Id INTEGER UNIQUE, 
 		Rating INTEGER, 
+	FOREIGN KEY (User_Id) REFERENCES User(Id) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (Media_Id) REFERENCES Media(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create Review, Username is unique so that every user may only create one review.
 CREATE TABLE IF NOT EXISTS Review(
 		Media_Id INTEGER, 
-        Username VARCHAR(24) UNIQUE, 
+        User_Id INTEGER UNIQUE, 
         Title VARCHAR(24) NOT NULL, 
 		Text VARCHAR(500) NOT NULL, 
+	FOREIGN KEY (User_Id) REFERENCES User(Id) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (Media_Id) REFERENCES Media(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -92,6 +102,7 @@ CREATE TABLE IF NOT EXISTS Review(
 -- Login credentials when reading from the database. Login information will be sent when required by the 
 -- Database, when creating a review or rating. 
 CREATE USER 'clientapp'@'localhost' IDENTIFIED BY 'qwerty';
+REVOKE ALL ON *.* FROM 'clientapp'@'%';					  -- Remove any default.
 GRANT SELECT ON MediaCollection.* TO 'clientapp'@'%';	  -- Grant read on all tables.
 REVOKE ALL ON MediaCollection.User FROM 'clientapp'@'%';  -- Removes select from User table, where passwords are stored.
 -- The admin logs on with write access, enabling the creation of accounts in User table, or to add
--- Build-TestData.sql
+++ Build-TestData.sql
@@ -11,25 +11,33 @@ INSERT INTO Genre (Name) VALUES ("Adventure");
 INSERT INTO Genre (Name) VALUES ("Singer/Songwriter");
 INSERT INTO Genre (Name) VALUES ("Education");
 
-INSERT INTO User (Name, Pass) VALUES ("Foo", "Foo");
-INSERT INTO User (Name, Pass) VALUES ("Bar", "Bar");
-INSERT INTO User (Name, Pass) VALUES ("FooBar", "FooBar");
+-- Authentication for uploading new content and submitting reviews, this is only visible to the
+-- server or the admin creating a new account.
+INSERT INTO Account (Name, Pass) VALUES ("Foo", "Foo");
+INSERT INTO Account (Name, Pass) VALUES ("Bar", "Bar");
+INSERT INTO Account (Name, Pass) VALUES ("FooBar", "FooBar");
+
+-- These should be created when the account is created, accessible read from all.
+INSERT INTO User (Account_Name) VALUES ("Foo");
+INSERT INTO User (Account_Name) VALUES ("Bar");
+INSERT INTO User (Account_Name) VALUES ("FooBar");
 
 -- Album = 1, Video = 2, E-Book = 3, Adding media.
-INSERT INTO Media (Mediatype_Id, Genre, Title, Year, Duration) VALUES (1, 3, "Imagine", 1971, 43);
-INSERT INTO Media (Mediatype_Id, Genre, Title, Year, Duration) VALUES (1, 3, "Rosenrot", 2005, 58);
-INSERT INTO Media (Mediatype_Id, Genre, Title, Year, Duration) VALUES (1, 3, "Reise, Reise", 2004, 34);
+INSERT INTO Media (Mediatype_Id, Genre_Id, Title, Year, Duration) VALUES (1, 3, "Imagine", 1971, 43);
+INSERT INTO Media (Mediatype_Id, Genre_Id, Title, Year, Duration) VALUES (1, 3, "Rosenrot", 2005, 58);
+INSERT INTO Media (Mediatype_Id, Genre_Id, Title, Year, Duration) VALUES (1, 3, "Reise, Reise", 2004, 34);
 
-INSERT INTO Media (Mediatype_Id, Genre, Title, Year, Duration) VALUES (2, 3, "The Shining", 1996, 180);
-INSERT INTO Media (Mediatype_Id, Genre, Title, Year, Duration) VALUES (2, 3, "Full Metal Jacket", 1998, 120);
-INSERT INTO Media (Mediatype_Id, Genre, Title, Year, Duration) VALUES (2, 3, "Interstellar", 1992, 90);
+INSERT INTO Media (Mediatype_Id, Genre_Id, Title, Year, Duration) VALUES (2, 3, "The Shining", 1996, 180);
+INSERT INTO Media (Mediatype_Id, Genre_Id, Title, Year, Duration) VALUES (2, 3, "Full Metal Jacket", 1998, 120);
+INSERT INTO Media (Mediatype_Id, Genre_Id, Title, Year, Duration) VALUES (2, 3, "Interstellar", 1992, 90);
 
-INSERT INTO Media (Mediatype_Id, Genre, Title, Year, Duration) VALUES (3, 4, "Databases", 2009, 681);
-INSERT INTO Media (Mediatype_Id, Genre, Title, Year, Duration) VALUES (3, 4, "Databases^2", 2011, 2975);
+INSERT INTO Media (Mediatype_Id, Genre_Id, Title, Year, Duration) VALUES (3, 4, "Databases", 2009, 681);
+INSERT INTO Media (Mediatype_Id, Genre_Id, Title, Year, Duration) VALUES (3, 4, "Databases^2", 2011, 2975);
 
 -- Adding media authors, Recommended to do this at least once for every media on insertion:
 INSERT INTO Contributor(Creator_Id, Media_Id) VALUES (2,2); -- Rammstein = Rosenrot
 INSERT INTO Contributor(Creator_Id, Media_Id) VALUES (2,3); -- Rammstein = Reise, Reise
 
-INSERT INTO Review (Media_Id, Username, Title, Text) VALUES (1, "KittyMeows", "Rammstein??", "Ist Sehr Gut. Keine Fragen."); 
+-- Review added by [1] = Foo:Foo, Second parameter.
+INSERT INTO Review (Media_Id, User_Id, Title, Text) VALUES (1, 1, "Rammstein??", "Ist Sehr Gut. Keine Fragen."); 
 
