f6d2bcd1ce0cdbd6c9562af9d77afb6123c753a9
softish <lazar.h.klincov@live.se>
2014-12-18 12:52:02 +0100

    mDB style

--- BuildDatabase.js
+++ BuildDatabase.js
@@ -15,87 +15,46 @@ use mediacollection
 
 
 // make collections
-
+// TODO: Make ref. to account ? if login is supposed to be available
 //Create User, Contains password and username for use in the post-review/post-rating procedures.
 db.createCollection("Account")
 // These test accounts are used in the creation of new Media/Reviews/Ratings
-db.Account.insert({Name : 'MrFoo', User : 'Foo', Pass : 'Foo' })
-db.Account.insert({Name : 'MrsBar', User : 'Bar', Pass : 'Bar' })
-db.Account.insert({Name : 'Alice', User : 'AUser', Pass : 'APass' })
-db.Account.insert({Name : 'Bob', User : 'BUser', Pass : 'BPass' })
+db.Account.insert({Name : 'MrFoo', User : 'Foo'})
+db.Account.insert({Name : 'MrsBar', User : 'Bar')
+
 // Use this account to log in during development, it has no preset reviews/ratings.
-db.Account.insert({Name : 'User42', User : 'User', Pass : 'Password' })
-
-// Create Director/Artist
-db.createCollection("Creator")
-db.Creator.insert({Name : "Rammstein"})
-
-// Adding index
-db.Creator.ensureIndex( { Name: 1 } )
-
-// Create Genre
-db.createCollection("Genre")
-db.Genre.insert({Name : "Rammstyle"})
-// TODO connect dem in good fashion
-
-// Create Media Types
-db.createCollection("Mediatype")
-// TODO: add base types!!! is this it?
-db.Mediatype.insert({Name : 'Album'})
-db.Mediatype.insert({Name : 'Movie'})
-db.Mediatype.insert({Name : 'E-Book'})
-db.Mediatype.insert({Name : 'Undefined'})
-// Genre defaults to undefined.
+db.Account.insert({Name : 'User42', User : 'User'})
 
 // Create Movie/Album
-db.createCollection("Media")
-
-//  needs more research
 db.Media.insert({ 
-	Title : 'Rosenrot', 
-	Creator : "Rammstein", // FK
-	Genre : "Rammstyle", 	// FK
+	Title : "Rosenrot", 
+	Creator : "Rammstein",	// will become list []
+	Genre : "Rammstyle",
 	Year : "2009",
 	Duration : "120",
-	Mediatype : "1",	// FK
-	AddedBy : ""
+	Review : [
+		{
+			User : "Bar", 
+			Title : "Excellent!", 
+			Review : "With deep flavor and class..." 
+		},
+		{
+			User : "Foo", 
+			Title : "Supberb.", 
+			Review : "Very Good. Such Wack."
+		}
+	],
+	Rating : [
+		{User: "Foo", Rating : "1"}, 
+		{User: "Bar", Rating : "5"}
+	],	// calc. avg. in app, prevent wrong values!
+	AddedBy : "Foo",
+	Mediatype : "Album"
 })
 // Ref 2 who added it!
 
 db.Media.ensureIndex( { Title: 1 } )
 db.Media.ensureIndex( { Mediatype_Id: 1 } )
-
-
-// researching how N:M tables are used in noSQL DB's
-db.createCollection("Contributor")
-
-// Create Rating, Username is unique so that every user may only create one rating.  
-db.createCollection("Rating")
-db.Rating.insert({User : "BUser", Rating : "2"})
-db.Rating.insert({User : "AUser", Rating : "5"})
-
-// Create Review, Username is unique so that every user may only create one review.
-db.createCollection("Review")
-db.Review.insert({
-	User : "Bar", 
-	Title : "Excellent!", 
-	Review : "With deep flavor and class..."
-})
-db.Review.insert({
-	User : "Foo", 
-	Title : "Supberb.", 
-	Review : "Very Good. Such Wack."
-})
-db.Review.insert({
-	User : "AUser", 
-	Title : "Naaa?", 
-	Review : "Sorry, not feeling it.."
-})
-db.Review.insert({
-	User : "BUser", 
-	Title : "Alright", 
-	Review : "Could have used more SQL.."
-})
 
 
 // show what has been made
